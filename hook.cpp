75b2a7b96f3716b9f896af42d9972ae8c68c504f
archibate <1931127624@qq.com>
2024-06-22 01:15:19 +0800

    updatemsvcfix

--- hook.cpp
+++ hook.cpp
@@ -81,7 +81,7 @@ extern "C" void *__libc_memalign(size_t align, size_t size) noexcept;
 #define REAL_LIBC(name) __libc_##name
 #define MAY_OVERRIDE_MALLOC 1
 #define MAY_SUPPORT_MEMALIGN 1
-
+#define RETURN_ADDRESS __builtin_return_address(0)
 #elif _MSC_VER
 static void *msvc_malloc(size_t size) noexcept {
     return HeapAlloc(GetProcessHeap(), 0, size);
@@ -108,10 +108,16 @@ static void *msvc_reallocarray(void *ptr, size_t nmemb,
 #define MAY_OVERRIDE_MALLOC 1
 #define MAY_SUPPORT_MEMALIGN 0
 
+#include <intrin.h>
+
+#pragma intrinsic(_ReturnAddress)
+#define RETURN_ADDRESS _ReturnAddress()
+
 #else
 #define REAL_LIBC(name) name
 #define MAY_OVERRIDE_MALLOC 0
 #define MAY_SUPPORT_MEMALIGN 0
+#define RETURN_ADDRESS ((void *)1)
 #endif
 
 #if MAY_OVERRIDE_MALLOC
@@ -120,7 +126,7 @@ extern "C" void *malloc(size_t size) noexcept {
     void *ptr = REAL_LIBC(malloc)(size);
     if (ena) {
         global.on(AllocOp::Malloc, ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -129,7 +135,7 @@ extern "C" void free(void *ptr) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Free, ptr, kNone, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -139,7 +145,7 @@ extern "C" void *calloc(size_t nmemb, size_t size) noexcept {
     void *ptr = REAL_LIBC(calloc)(nmemb, size);
     if (ena) {
         global.on(AllocOp::Malloc, ptr, nmemb * size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -149,10 +155,10 @@ extern "C" void *realloc(void *ptr, size_t size) noexcept {
     void *new_ptr = REAL_LIBC(realloc)(ptr, size);
     if (ena) {
         global.on(AllocOp::Malloc, new_ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
         if (new_ptr) {
             global.on(AllocOp::Free, ptr, kNone, kNone,
-                      __builtin_return_address(0));
+                      RETURN_ADDRESS);
         }
     }
     return new_ptr;
@@ -163,10 +169,10 @@ extern "C" void *reallocarray(void *ptr, size_t nmemb, size_t size) noexcept {
     void *new_ptr = REAL_LIBC(reallocarray)(ptr, nmemb, size);
     if (ena) {
         global.on(AllocOp::Malloc, new_ptr, nmemb * size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
         if (new_ptr) {
             global.on(AllocOp::Free, ptr, kNone, kNone,
-                      __builtin_return_address(0));
+                      RETURN_ADDRESS);
         }
     }
     return new_ptr;
@@ -188,7 +194,7 @@ extern "C" void *valloc(size_t size) noexcept {
         size_t pagesize = 0;
 #endif
         global.on(AllocOp::Malloc, ptr, size, pagesize,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -198,7 +204,7 @@ extern "C" void *memalign(size_t align, size_t size) noexcept {
     void *ptr = REAL_LIBC(memalign)(align, size);
     if (ena) {
         global.on(AllocOp::Malloc, ptr, size, align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -208,7 +214,7 @@ extern "C" void *aligned_alloc(size_t align, size_t size) noexcept {
     void *ptr = REAL_LIBC(memalign)(align, size);
     if (ena) {
         global.on(AllocOp::Malloc, ptr, size, align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -218,7 +224,7 @@ extern "C" int posix_memalign(void **memptr, size_t align, size_t size) noexcept
     void *ptr = REAL_LIBC(memalign)(align, size);
     if (ena) {
         global.on(AllocOp::Malloc, *memptr, size, align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     int ret = 0;
     if (!ptr) {
@@ -235,7 +241,7 @@ void operator delete(void *ptr) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, kNone, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -244,7 +250,7 @@ void operator delete[](void *ptr) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, kNone, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -253,7 +259,7 @@ void operator delete(void *ptr, std::nothrow_t const &) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, kNone, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -262,7 +268,7 @@ void operator delete[](void *ptr, std::nothrow_t const &) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, kNone, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -271,7 +277,7 @@ void *operator new(size_t size) noexcept(false) {
     EnableGuard ena;
     void *ptr = REAL_LIBC(malloc)(size);
     if (ena) {
-        global.on(AllocOp::New, ptr, size, kNone, __builtin_return_address(0));
+        global.on(AllocOp::New, ptr, size, kNone, RETURN_ADDRESS);
     }
     if (ptr == nullptr) {
         throw std::bad_alloc();
@@ -284,7 +290,7 @@ void *operator new[](size_t size) noexcept(false) {
     void *ptr = REAL_LIBC(malloc)(size);
     if (ena) {
         global.on(AllocOp::NewArray, ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     if (ptr == nullptr) {
         throw std::bad_alloc();
@@ -296,7 +302,7 @@ void *operator new(size_t size, std::nothrow_t const &) noexcept {
     EnableGuard ena;
     void *ptr = REAL_LIBC(malloc)(size);
     if (ena) {
-        global.on(AllocOp::New, ptr, size, kNone, __builtin_return_address(0));
+        global.on(AllocOp::New, ptr, size, kNone, RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -306,7 +312,7 @@ void *operator new[](size_t size, std::nothrow_t const &) noexcept {
     void *ptr = REAL_LIBC(malloc)(size);
     if (ena) {
         global.on(AllocOp::NewArray, ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -316,7 +322,7 @@ void operator delete(void *ptr, size_t size) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -325,7 +331,7 @@ void operator delete[](void *ptr, size_t size) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, size, kNone,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -337,7 +343,7 @@ void operator delete(void *ptr, std::align_val_t align) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, kNone, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -346,7 +352,7 @@ void operator delete[](void *ptr, std::align_val_t align) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, kNone, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -355,7 +361,7 @@ void operator delete(void *ptr, size_t size, std::align_val_t align) noexcept {
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -365,7 +371,7 @@ void operator delete[](void *ptr, size_t size,
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -375,7 +381,7 @@ void operator delete(void *ptr, std::align_val_t align,
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::Delete, ptr, kNone, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -385,7 +391,7 @@ void operator delete[](void *ptr, std::align_val_t align,
     EnableGuard ena;
     if (ena) {
         global.on(AllocOp::DeleteArray, ptr, kNone, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     REAL_LIBC(free)(ptr);
 }
@@ -395,7 +401,7 @@ void *operator new(size_t size, std::align_val_t align) noexcept(false) {
     void *ptr = REAL_LIBC(memalign)((size_t)align, size);
     if (ena) {
         global.on(AllocOp::New, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     if (ptr == nullptr) {
         throw std::bad_alloc();
@@ -408,7 +414,7 @@ void *operator new[](size_t size, std::align_val_t align) noexcept(false) {
     void *ptr = REAL_LIBC(memalign)((size_t)align, size);
     if (ena) {
         global.on(AllocOp::NewArray, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     if (ptr == nullptr) {
         throw std::bad_alloc();
@@ -422,7 +428,7 @@ void *operator new(size_t size, std::align_val_t align,
     void *ptr = REAL_LIBC(memalign)((size_t)align, size);
     if (ena) {
         global.on(AllocOp::New, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
@@ -433,7 +439,7 @@ void *operator new[](size_t size, std::align_val_t align,
     void *ptr = REAL_LIBC(memalign)((size_t)align, size);
     if (ena) {
         global.on(AllocOp::NewArray, ptr, size, (size_t)align,
-                  __builtin_return_address(0));
+                  RETURN_ADDRESS);
     }
     return ptr;
 }
--- main.cpp
+++ main.cpp
@@ -4,23 +4,27 @@
 #include <thread>
 #include <unordered_set>
 #include <vector>
+#if HAS_TSL
 #include <tsl/robin_set.h>
+#endif
+#if HAS_ABSL
 #include <absl/container/flat_hash_set.h>
 #include <absl/container/node_hash_set.h>
+#endif
 
 using namespace std::chrono_literals;
 
 int main() {
     { // vector
         std::vector<int> a;
-        for (int i = 0; i < 400; i++) {
+        for (int i = 0; i < 200; i++) {
             a.push_back(i);
             std::this_thread::sleep_for(1us);
         }
     }
     { // deque
         std::deque<int> a;
-        for (int i = 0; i < 400; i++) {
+        for (int i = 0; i < 200; i++) {
             a.push_back(i);
             std::this_thread::sleep_for(1us);
         }
@@ -46,6 +50,7 @@ int main() {
             std::this_thread::sleep_for(1us);
         }
     }
+#if HAS_TSL
     { // robin_set
         tsl::robin_set<int> a;
         for (int i = 0; i < 100; i++) {
@@ -53,6 +58,8 @@ int main() {
             std::this_thread::sleep_for(1us);
         }
     }
+#endif
+#if HAS_ABSL
     { // flat_hash_set
         absl::flat_hash_set<int> a;
         for (int i = 0; i < 100; i++) {
@@ -67,5 +74,6 @@ int main() {
             std::this_thread::sleep_for(1us);
         }
     }
+#endif
     return 0;
 }
